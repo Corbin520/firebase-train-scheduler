5ed05bf431c3a55630000e45ac2a2bd7488bbe8d
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-03-19 22:17:11 -0600

    everything working with firebase and code

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,4 @@
-
+// Initialize Firebase, so that it can communicate to the database
 var config = {
   apiKey: "AIzaSyAdM_G2727a9G5CPWGQ8bgKfhMynCsJdTo",
   authDomain: "train-project-homework-71f41.firebaseapp.com",
@@ -8,16 +8,16 @@ var config = {
   messagingSenderId: "1048882482973"
 };
 
-
+// initialize the config
 firebase.initializeApp(config);
 
 
-
+// Reference to the database service
 var database = firebase.database();
 
 
 
-
+// move up in order when done
 $("#submit-button").on("click", function (event) {
   var trainText = $("#train-name").val().trim();
   var destinationText = $("#destination").val().trim();
@@ -32,14 +32,44 @@ $("#submit-button").on("click", function (event) {
   });
 })
 
+   // Assumptions
+   var tFrequency = 3;
 
+   // Time is 3:30 AM
+   var firstTime = "03:30";
+
+   
+// firebase listener, if a change has been made, run this function.
+// grab data from firebase and put it back down on our page
 firebase.database().ref().on("child_added", function (snapshot) {
   var train = snapshot.val().train;
   var destination = snapshot.val().destination;
   var frequency = snapshot.val().frequency;
   var start = snapshot.val().start;
 
+  // calculations of the arrival etc
+  var firstTimeConverted = moment(start, "HH:mm").subtract(1, "years");
+  console.log(firstTimeConverted);
 
+  // Current Time
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % tFrequency;
+  console.log(tRemainder);
+
+  // Minute Until Train
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
   var tr = $("<tr>").append(
     $("<td>").text(train),
@@ -53,7 +83,10 @@ firebase.database().ref().on("child_added", function (snapshot) {
   $("#train-table > tbody").append(tr);
 
   
-
+  // always use snapshot.val() so you get what you want returned, Otherwise you will get an object
+  // with information that you wont be able to decipher. You can also use .html to print it to the
+  // screen
 
 })
 
+// https://www.youtube.com/watch?v=ZWH19t4ujRA (example of pushing up data, and pulling it back down)
