5b88e0ce21f60f65e69d2c8d03001e9957b5dd72
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-03-19 21:13:39 -0600

    communicating with the DB, pushing it back down

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,59 @@
+
+var config = {
+  apiKey: "AIzaSyAdM_G2727a9G5CPWGQ8bgKfhMynCsJdTo",
+  authDomain: "train-project-homework-71f41.firebaseapp.com",
+  databaseURL: "https://train-project-homework-71f41.firebaseio.com",
+  projectId: "train-project-homework-71f41",
+  storageBucket: "",
+  messagingSenderId: "1048882482973"
+};
+
+
+firebase.initializeApp(config);
+
+
+
+var database = firebase.database();
+
+
+
+
+$("#submit-button").on("click", function (event) {
+  var trainText = $("#train-name").val().trim();
+  var destinationText = $("#destination").val().trim();
+  var frequencyText = $("#frequency").val().trim();
+  var startText = $("#start-date").val().trim();
+  
+  database.ref().push({
+    train: trainText,
+    destination: destinationText,
+    frequency: frequencyText,
+    start: startText,
+  });
+})
+
+
+firebase.database().ref().on("child_added", function (snapshot) {
+  var train = snapshot.val().train;
+  var destination = snapshot.val().destination;
+  var frequency = snapshot.val().frequency;
+  var start = snapshot.val().start;
+
+
+
+  var tr = $("<tr>").append(
+    $("<td>").text(train),
+    $("<td>").text(destination),
+    $("<td>").text(frequency),
+    $("<td>").text(nextTrain),
+    $("<td>").text(tMinutesTillTrain),
+
+  );
+
+  $("#train-table > tbody").append(tr);
+
+  
+
+
+})
+
